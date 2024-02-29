ae679806e540eacd3bc95efa8ea70437ff791129
owino600 <owinodaniel600@gmail.com>
2024-02-29 18:03:50 +0300

    .\sales.py

--- sales.py
+++ sales.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-from sqlalchemy import String, Column, Primarykey, integer
-class sales(BaseModel, Base):
-    """representation of sales """
-    _tablename_ = 'sales '
-sales_id = Column(String(30), Primarykey ('sales.id'), nullable=False )
-opening_stock=Column(String(50), nullable=False)
-product_name=Column(String(50), nullable=False)
-quantity_sold=Column(integer, nullable=False)
-revenue=Column(float, nullable=False)
-closing_stock=Column(float, nullable=False)
-def calculate_revenue(self):
-    return self.quantity_sold * self.revenu
-def calculate_closing_stock(self):
-     return float(self.opening_stock) - self.quantity_sold
- 
+from sqlalchemy import String, Column, Integer, Float
+from sqlalchemy.ext.declarative import declarative_base
 
-            
+Base = declarative_base()
+
+class Sales(Base):
+    """Representation of sales"""
+    __tablename__ = 'sales'
+
+    sales_id = Column(String(30), primary_key=True, nullable=False)
+    opening_stock = Column(String(50), nullable=False)
+    product_name = Column(String(50), nullable=False)
+    quantity_sold = Column(Integer, nullable=False)
+    revenue = Column(Float, nullable=False)
+    closing_stock = Column(Float, nullable=False)
+
+    def calculate_revenue(self):
+        return self.quantity_sold * self.revenue
+
+    def calculate_closing_stock(self):
+        return float(self.opening_stock) - self.quantity_sold            
